ea37c33be87845b46d40045ff714c3a23353eeaf
luctfyal12 <luctfyalkatiri08@gmail.com>
2023-07-30 01:31:30 +0700

    commit

---
+++ src/js/script.js
@@ -0,0 +1,63 @@
+const openPopup = document.getElementById("btn1")
+const content1 = document.getElementById("content")
+const popUp1 = document.getElementById("popup")
+
+openPopup.addEventListener('click', () => {
+    if(popUp1.style.display = 'none') {
+        popUp1.style.display = 'block';
+        content1.style.display = 'none';
+        console.log("1")
+    } else {
+    }
+})
+
+const scoreTap1 = document.getElementById("tap1")
+const scoreTap2 = document.getElementById("tap2")
+const scoreTap3 = document.getElementById("tap3")
+const scoreTap4 = document.getElementById("tap4")
+const scoreTap5 = document.getElementById("tap5")
+
+scoreTap1.addEventListener('click', () => {
+    if(scoreTap1.style.background = 'hsla(216, 12%, 54%, 0.1)') {
+        scoreTap1.style.background = 'hsl(25, 97%, 53%)';
+        scoreTap1.style.color = 'white';
+    } else {
+
+    }
+})
+
+scoreTap2.addEventListener('click', () => {
+    if(scoreTap2.style.background = 'hsla(216, 12%, 54%, 0.1)') {
+        scoreTap2.style.background = 'hsl(25, 97%, 53%)';
+        scoreTap2.style.color = 'white';
+    } else {
+
+    }
+})
+
+scoreTap3.addEventListener('click', () => {
+    if(scoreTap3.style.background = 'hsla(216, 12%, 54%, 0.1)') {
+        scoreTap3.style.background = 'hsl(25, 97%, 53%)';
+        scoreTap3.style.color = 'white';
+    } else {
+
+    }
+})
+
+scoreTap4.addEventListener('click', () => {
+    if(scoreTap4.style.background = 'hsla(216, 12%, 54%, 0.1)') {
+        scoreTap4.style.background = 'hsl(25, 97%, 53%)';
+        scoreTap4.style.color = 'white';
+    } else {
+
+    }
+})
+
+scoreTap5.addEventListener('click', () => {
+    if(scoreTap5.style.background = 'hsla(216, 12%, 54%, 0.1)') {
+        scoreTap5.style.background = 'hsl(25, 97%, 53%)';
+        scoreTap5.style.color = 'white';
+    } else {
+
+    }
+})
