d4b729f59cbb27e42f7ed8eafa8d0fb1828825ed
luctfyal12 <luctfyalkatiri08@gmail.com>
2023-07-30 01:18:33 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'overpass': ['Overpass'],
+    },
+    extend: {
+      colors: {
+        'dark-blue': 'hsl(216, 12%, 8%)',
+        'dark-blue-low': 'hsla(216, 12%, 8%, 0.1)',
+        'dark-grey': 'hsl(213, 19%, 18%)',
+        'medium-grey': 'hsla(216, 12%, 54%, 0.1)',
+        'light-grey': 'hsl(217, 12%, 63%)',
+        'orange-muda': 'hsl(25, 97%, 53%)',
+      }
+    },
+  },
+  plugins: [],
+}
+
